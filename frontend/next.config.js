06238e0732e3104ddc721d9a2bb05e9c4f51b094
Freddy Song <fredsong99@gmail.com>
2024-08-04 18:52:18 -0700

    fix: vercel v2

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,7 @@
-// next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     reactStrictMode: true,
-  }
+    output: 'standalone',
+  };
+  
+  module.exports = nextConfig;
